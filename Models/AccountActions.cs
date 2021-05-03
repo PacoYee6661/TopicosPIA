4d8b95c012df70463a22a1b74847f1b83090ebbb
Francisco Daniel Martinez Yee <60910785+PacoYee6661@users.noreply.github.com>
2021-05-03 10:39:02 -0500

    Agregadas las ventanas finales
    Aun falta darle diseno, quitar labels y agregar botones, crear pop ups y CSS en general

---
+++ Models/AccountActions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Topicos.Models
+{
+    public class AccountActions
+    {
+        public int idTicket { get; set; }
+        //Puede que se necesiten cambiar a char
+        public string descripcion { get; set; }
+        public string detalles { get; set; }
+        public string status { get; set; }
+    }
+}
---
+++ Models/AdminActions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Topicos.Models
+{
+    public class AdminActions
+    {
+        public int idTicket { get; set; }
+        //Puede que se necesiten cambiar a char
+        public string descripcion { get; set; }
+        public string detalles { get; set; }
+        public string solucion { get; set; }
+        public string status { get; set; }
+    }
+
+}
